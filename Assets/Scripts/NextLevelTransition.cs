43865a0449de13417cfe9965afda3d1de15fc9d7
aBacIdEe <91438467+aBacIdEe@users.noreply.github.com>
2023-06-17 15:37:41 -0400

    Put scene transitions in (not seamless)

--- Assets/Scripts/NextLevelTransition.cs
+++ Assets/Scripts/NextLevelTransition.cs
@@ -1,14 +1,46 @@
-using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NextLevelTransition : MonoBehaviour
+public class Example : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other)
+
+    bool isCurrentlyColliding;
+
+    void OnCollisionEnter(Collision col) {
+        isCurrentlyColliding = true;
+    }
+ 
+    void OnCollisionExit(Collision col) {
+        isCurrentlyColliding = false;
+    }
+
+    void Update()
     {
-        if (other.gameObject.tag == "Player")
+        // Press the space key to start coroutine
+        if (isCurrentlyColliding)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            // Use a coroutine to load the Scene in the background
+            StartCoroutine(LoadYourAsyncScene());
+        }
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+        
+        Scene scene = SceneManager.GetActiveScene();
+        string newName = "Scene" + (scene.name[5] - '0' + 1);
+        Debug.Log(newName);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(newName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
         }
     }
 }
